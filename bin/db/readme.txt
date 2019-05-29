bd347347e2d3c8e5bba714b98ff4b2826e8d665a
Duc <ntanduc94@outlook.com>
2019-05-30 00:22:18 +0700

    don't know where is /bin

--- bin/db/readme.txt
+++
@@ -1,4 +0,0 @@
-create a db with the same file name
-=> import that file
-=> done
-Notes: that file include data
--- src/main/java/com/demo/services/ProductService.java
+++ src/main/java/com/demo/services/ProductService.java
@@ -1,17 +1,17 @@
-package com.demo.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.demo.entities.Product;
-
-public interface ProductService extends CrudRepository<Product, Integer>{
-
-//	public Iterable<Product> findAll();
+//package com.demo.services;
 //
-//	public Product find(int id);
+//import org.springframework.data.repository.CrudRepository;
 //
-//	public Product save(Product product);
+//import com.demo.entities.Product;
 //
-//	public void delete(int id);
-
-}
+//public interface ProductService extends CrudRepository<Product, Integer>{
+//
+////	public Iterable<Product> findAll();
+////
+////	public Product find(int id);
+////
+////	public Product save(Product product);
+////
+////	public void delete(int id);
+//
+//}
