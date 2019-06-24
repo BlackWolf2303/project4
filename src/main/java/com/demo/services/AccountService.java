471dbab2581fb4fbabd6003333e0dd8ce4c23d0f
Duc <ntanduc94@outlook.com>
2019-06-24 21:31:31 +0700

    user/admin

--- src/main/java/com/demo/services/AccountService.java
+++ src/main/java/com/demo/services/AccountService.java
@@ -2,6 +2,12 @@ package com.demo.services;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.demo.entities.Account;
+
 public interface AccountService extends UserDetailsService {
+
+	void save(Account account);
+
+	Account findByUsername(String username);
 
 }
